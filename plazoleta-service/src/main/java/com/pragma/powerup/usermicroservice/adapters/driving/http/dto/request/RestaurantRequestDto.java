f16cde63021c04e5564fc396d52a44f942d9792c
Heinner Vega <heinnervega20@gmail.com>
2023-04-29 16:25:21 -0500

    HU2 Add minimum validations for restaurant

--- plazoleta-service/src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/dto/request/RestaurantRequestDto.java
+++ plazoleta-service/src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/dto/request/RestaurantRequestDto.java
@@ -1,5 +1,7 @@
 package com.pragma.powerup.usermicroservice.adapters.driving.http.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,12 +10,18 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 public class RestaurantRequestDto {
-    private Long id;
+    @NotBlank(message = "Field 'name' it's required")
     private String name;
+    @NotBlank(message = "Field 'address' it's required")
     private String address;
+    @NotBlank(message = "Field 'idOwner' it's required")
     private String idOwner;
 
+    @Pattern(regexp = "^\\+?57\\s(3[0-2]|7[0-1])\\d{8}$", message = "Field 'numberPhone' must be a valid number phone. Enter the format +57 3...")
+    @NotBlank(message = "Field 'phoneNumber' it's required")
     private String phone;
+    @NotBlank(message = "Field 'urlLogo' it's required")
     private String urlLogo;
+    @NotBlank(message = "Field 'NIT' it's required")
     private String nit;
 }
